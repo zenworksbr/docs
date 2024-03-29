b3c391788713556c2b0b55b8fe6b12ebc5f91469
r47orr <razoroller529@gmail.com>
2024-03-29 04:31:15 -0300

    atualziar nome do projeto Sphinx

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Zenworks'
+project = 'Zenworks - Docs'
 copyright = '2024, Zenworks'
 author = 'Razor'
 release = '0.1'
